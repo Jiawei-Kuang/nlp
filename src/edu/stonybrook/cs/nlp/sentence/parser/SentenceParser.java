5b2df0ed747b3d8d9fef99c70fd69e83dd1f9113
Jiawei <jiawei.kuang0818@gmail.com>
2014-11-09 21:38:36 -0500

    indentation

--- src/edu/stonybrook/cs/nlp/sentence/parser/SentenceParser.java
+++ src/edu/stonybrook/cs/nlp/sentence/parser/SentenceParser.java
@@ -1,9 +1,19 @@
 package edu.stonybrook.cs.nlp.sentence.parser;
+
+/**
+ * 
+ * @author Jiawei Kuang
+ * @version 1.0
+ * @since Nov. 8, 2014
+ */
+
+//TODO use attempto library to parse sentence 
 public class SentenceParser {
-	public String parseToDRS(String text) {
-		return "DRS";
-	}
-	public String parseToFOL(String text) {
-		return "FOL";
-	}
+    
+    public String parseToDRS(String text) {
+        return "DRS";
+    }
+    public String parseToFOL(String text) {
+        return "FOL";
+    }
 }
--- src/edu/stonybrook/cs/nlp/sentence/parser/SentenceParserHandler.java
+++ src/edu/stonybrook/cs/nlp/sentence/parser/SentenceParserHandler.java
@@ -4,13 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.stonybrook.cs.nlp.sentence.Sentence;
 
+/**
+ * 
+ * @author Jiawei Kuang
+ * @version 1.0
+ * @since Nov. 8, 2014
+ */
+
 public class SentenceParserHandler {
-	
-	@Autowired
-	private SentenceParser sentenceParser;
-	
-	public void parse(Sentence sentence) {
-		sentence.setDrs(sentenceParser.parseToDRS(sentence.getSentence()));
-		sentence.setFol(sentenceParser.parseToFOL(sentence.getSentence()));
-	}
+    
+    @Autowired
+    private SentenceParser sentenceParser;
+    
+    public void parse(Sentence sentence) {
+        sentence.setDrs(sentenceParser.parseToDRS(sentence.getSentence()));
+        sentence.setFol(sentenceParser.parseToFOL(sentence.getSentence()));
+    }
 }
