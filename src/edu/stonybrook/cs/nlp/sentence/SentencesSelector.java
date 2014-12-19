0115b11bfac464e195e57c138db3a705ca38778b
Jiawei <jiawei.kuang0818@gmail.com>
2014-12-18 20:59:59 -0500

    clean commented code

--- src/edu/stonybrook/cs/nlp/sentence/SentencesSelector.java
+++ src/edu/stonybrook/cs/nlp/sentence/SentencesSelector.java
@@ -13,7 +13,6 @@ import jiawei.kuang.common.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.stonybrook.cs.nlp.common.Constant.InputSentence;
-import edu.stonybrook.cs.nlp.exception.SentenceInvalidException;
 
 /**
  * This class is used to get and clean the informations
@@ -39,8 +38,7 @@ public class SentencesSelector {
      * @param request
      * @return List<Sentence>
      */
-    public List<Sentence> getSentences(HttpServletRequest request) 
-            throws SentenceInvalidException{
+    public List<Sentence> getSentences(HttpServletRequest request) {
         List<Sentence> sentences = new ArrayList<>();
         String submit = request.getParameter("submit");
         if (submit == null) {
@@ -54,10 +52,6 @@ public class SentencesSelector {
             List<List<String>> exceptionsList = partitionExceptions(exceptions);
             int numOfSentences = setenceParameters.length;
             for (int i = 0; i < numOfSentences; i++) {
-//                if (inputSentences[i].isEmpty()) {
-//                    throw new SentenceInvalidException(
-//                            emptyInputExceptionMessage(i + 1));
-//                }
                 inputSentences[i] = inputSentences[i].trim();
                 Sentence sentence = new Sentence(inputSentences[i], i + 1, 
                         setenceParameters[i], exceptionsList.get(i));
@@ -65,65 +59,6 @@ public class SentencesSelector {
             }
         }
         return sentences;
-        
-        /*
-        List<Sentence> sentences = new ArrayList<>();
-        if (request.getParameterValues(InputSentence.SENTENCE_PARAMETER) == null) {
-            return sentences;
-        }
-        
-        String[] inputSentences         = request.getParameterValues(InputSentence.INPUT_SENTENCE);
-        String[] ifSentences            = request.getParameterValues(InputSentence.IF_SENTENCE);
-        String[] thenSentences          = request.getParameterValues(InputSentence.THEN_SENTENCE);
-        String[] setenceParameters      = request.getParameterValues(InputSentence.SENTENCE_PARAMETER);
-        String[] sentenceTypes          = request.getParameterValues(InputSentence.SENTENCE_TYPE);
-        String[] questionTypes          = request.getParameterValues(InputSentence.QUESTION_TYPE);
-        String[] exceptions             = request.getParameterValues(InputSentence.EXCEPTION);
-        List<List<String>> exceptionsList = partitionExceptions(exceptions);
-        
-        int numOfSentences = setenceParameters.length;
-        for (int i = 0; i < numOfSentences; i++) {
-            Sentence sentence = new Sentence();
-            sentence.setNum(i + 1);
-            sentence.setParameter(setenceParameters[i]);
-            sentence.setExceptions(exceptionsList.get(i));
-            switch (sentenceTypes[i]) {
-            case InputSentence.Type.SENTENCE :
-                if (inputSentences[i].isEmpty()) {
-                    throw new SentenceInvalidException(
-                            emptyInputExceptionMessage(i + 1));
-                }
-                sentence.setSentence(inputSentences[i].trim());
-                break;
-            case InputSentence.Type.RULE :
-                if (ifSentences[i].isEmpty() || thenSentences[i].isEmpty()) {
-                    throw new SentenceInvalidException(
-                            emptyInputExceptionMessage(i + 1));
-                }
-                StringBuffer sb = new StringBuffer();
-                sb.append("If ");
-                sb.append(ifSentences[i].trim());
-                sb.append(", then ");
-                sb.append(thenSentences[i].trim());
-                sb.append(".");
-                sentence.setSentence(sb.toString());
-                break;
-            case InputSentence.Type.QUESTION :
-                if (inputSentences[i].isEmpty()) {
-                    throw new SentenceInvalidException(
-                            emptyInputExceptionMessage(i + 1));
-                }
-                StringBuffer questionSentence = new StringBuffer();
-                questionSentence.append(questionTypes[i] + " ");
-                questionSentence.append(inputSentences[i].trim() + "?");
-                sentence.setSentence(questionSentence.toString());
-                break;
-            }
-            sentences.add(sentence);
-        }
-        
-        return sentences;
-        */
     }
     
     private List<Sentence> getParagraphSentences(HttpServletRequest request) {
@@ -141,20 +76,6 @@ public class SentencesSelector {
         }
         return sentences;
     }
-    
-    /**
-     * 
-     * @param num, the sequence number of input sentence
-     * @return the ordinal number of input number
-     */
-//    private String emptyInputExceptionMessage(int num) {
-//        Formatter formatter = new Formatter();
-//        formatter.format("The %s input sentence is not completed", 
-//                numUtil.convertNumToOrdinal(num));
-//        String ret = formatter.toString();
-//        formatter.close();
-//        return ret;
-//    }
     
     private List<List<String>> partitionExceptions(String[] exceptions) {
         List<List<String>> retList = new ArrayList<List<String>>();
