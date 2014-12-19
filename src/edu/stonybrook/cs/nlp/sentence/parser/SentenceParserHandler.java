1dd663f8d0396922ca487d954e1a9bd7eb7cf707
Jiawei <jiawei.kuang0818@gmail.com>
2014-12-18 21:02:01 -0500

    clean code

--- src/edu/stonybrook/cs/nlp/sentence/parser/SentenceParserHandler.java
+++ src/edu/stonybrook/cs/nlp/sentence/parser/SentenceParserHandler.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import ch.uzh.ifi.attempto.ape.ACEParserException;
 import ch.uzh.ifi.attempto.ape.Message;
+import ch.uzh.ifi.attempto.ape.MessageContainer;
 import edu.stonybrook.cs.nlp.sentence.Sentence;
 
 /**
@@ -19,29 +20,34 @@ public class SentenceParserHandler {
     private SentenceParser sentenceParser;
     
     public void parse(Sentence sentence) {
+        String sentenceString = sentence.getSentence();
         try {
-            sentence.setDrs(sentenceParser.parseToDRS(sentence.getSentence()));
+            String drs = sentenceParser.parseToDRS(sentenceString);
+            sentence.setDrs(drs);
             sentence.setDrsParseSuccess(true);
         } catch (ACEParserException e) {
-            StringBuilder sb = new StringBuilder();
-            for (Message m : e.getMessageContainer().getErrorMessages()) {
-                sb.append(m.toString());
-                sb.append('\n');
-            }
-            sentence.setDrs(sb.toString());
+            String errMsg = getACEParserExceptionErrorMessage(e);
+            sentence.setDrs(errMsg);
             sentence.setDrsParseSuccess(false);
         }
         try {
-            sentence.setFol(sentenceParser.parseToFOL(sentence.getSentence()));
+            String fol = sentenceParser.parseToFOL(sentenceString);
+            sentence.setFol(fol);
             sentence.setFolParseSuccess(true);
         } catch (ACEParserException e) {
-            StringBuilder sb = new StringBuilder();
-            for (Message m : e.getMessageContainer().getErrorMessages()) {
-                sb.append(m.toString());
-                sb.append('\n');
-            }
-            sentence.setFol(sb.toString());
+            String errMsg = getACEParserExceptionErrorMessage(e);
+            sentence.setFol(errMsg);
             sentence.setFolParseSuccess(false);
         }
+    }
+    
+    private String getACEParserExceptionErrorMessage(ACEParserException e) {
+        StringBuilder sb = new StringBuilder();
+        MessageContainer messageContainer = e.getMessageContainer();
+        for (Message m : messageContainer.getErrorMessages()) {
+            sb.append(m.toString());
+            sb.append('\n');
+        }
+        return sb.toString();
     }
 }
