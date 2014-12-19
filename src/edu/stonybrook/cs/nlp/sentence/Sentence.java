eb8faa4000164ec6f2f841f16b4666dc5c259c23
Jiawei <jiawei.kuang0818@gmail.com>
2014-12-18 21:26:41 -0500

    move same code into a common function init

--- src/edu/stonybrook/cs/nlp/sentence/Sentence.java
+++ src/edu/stonybrook/cs/nlp/sentence/Sentence.java
@@ -16,10 +16,10 @@ import edu.stonybrook.cs.nlp.common.Constant.InputSentence;
  */
 
 public class Sentence {
-    
     private Integer num;
     private String sentence;
     private String parameter;
+    // This should change to List<Integer> later
     private List<String> exceptions;
     private String drs;
     private String fol;
@@ -30,63 +30,82 @@ public class Sentence {
     }
     
     public Sentence(String sentence, int num) {
-        this.sentence = sentence;
-        this.num = num;
+        init(sentence, num);
         this.parameter = InputSentence.Parameter.STRICT;
         this.exceptions = new ArrayList<>();
     }
+    
     public Sentence(String sentence, int num, String parameter, List<String> exceptions) {
-        this.sentence = sentence;
-        this.num = num;
+        init(sentence, num);
         this.parameter = parameter;
         this.exceptions = exceptions;
+    }
+    
+    private void init(String sentence, int num) {
+        this.sentence = sentence;
+        this.num = num;
     }
     
     public Integer getNum() {
         return num;
     }
+    
     public void setNum(Integer num) {
         this.num = num;
     }
+    
     public String getSentence() {
         return sentence;
     }
+    
     public void setSentence(String sentence) {
         this.sentence = sentence;
     }
+    
     public String getParameter() {
         return parameter;
     }
+    
     public void setParameter(String parameter) {
         this.parameter = parameter;
     }
+    
     public List<String> getExceptions() {
         return exceptions;
     }
+    
     public void setExceptions(List<String> excepts) {
         this.exceptions = excepts;
     }
+    
     public String getDrs() {
         return drs;
     }
+    
     public void setDrs(String drs) {
         this.drs = drs;
     }
+    
     public String getFol() {
         return fol;
     }
+    
     public void setFol(String fol) {
         this.fol = fol;
     }
+    
     public boolean isDrsParseSuccess() {
         return drsParseSuccess;
     }
+    
     public void setDrsParseSuccess(boolean drsParseSuccess) {
         this.drsParseSuccess = drsParseSuccess;
     }
+    
     public boolean isFolParseSuccess() {
         return folParseSuccess;
     }
+    
     public void setFolParseSuccess(boolean folParseSuccess) {
         this.folParseSuccess = folParseSuccess;
     }
