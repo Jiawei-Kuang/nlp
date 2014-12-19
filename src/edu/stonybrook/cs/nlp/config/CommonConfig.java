4ccf1057749f0023041947c06c91cf40b74ac50e
Jiawei <jiawei.kuang0818@gmail.com>
2014-12-18 21:24:56 -0500

    remove unused bean NumUtil

--- src/edu/stonybrook/cs/nlp/config/CommonConfig.java
+++ src/edu/stonybrook/cs/nlp/config/CommonConfig.java
@@ -1,6 +1,5 @@
 package edu.stonybrook.cs.nlp.config;
 
-import jiawei.kuang.common.util.NumUtil;
 import jiawei.kuang.common.util.StringUtil;
 
 import org.springframework.context.annotation.Bean;
@@ -8,11 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfig {
-    @Bean
-    public NumUtil numUtil() {
-        return new NumUtil();
-    }
-    
     @Bean
     public StringUtil stringUtil() {
         return new StringUtil();
